8a35fb98c625c574df0b54473d3004d212db1929
Pluto <devvaannsh@gmail.com>
2024-12-09 00:21:29 +0530

    feat: add button to status bar

--- main.js
+++ main.js
@@ -1,44 +1,33 @@
-/*global define, brackets, $ */
-
-// See detailed docs in https://docs.phcode.dev/api/creating-extensions
-
-// A good place to look for code examples for extensions: https://github.com/phcode-dev/phoenix/tree/main/src/extensions/default
-
-// A simple extension that adds an entry in "file menu> hello world"
+// Extension that is used for cursor settings, like changing the style of the cursor, the width of the cursor block or cursor blinking style
 define(function (require, exports, module) {
     "use strict";
-
     // Brackets modules
     const AppInit = brackets.getModule("utils/AppInit"),
         DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
         Dialogs = brackets.getModule("widgets/Dialogs"),
         CommandManager = brackets.getModule("command/CommandManager"),
-        Menus = brackets.getModule("command/Menus");
+        Menus = brackets.getModule("command/Menus"),
+        StatusBar = brackets.getModule("widgets/StatusBar");
 
     // Function to run when the menu item is clicked
-    function handleHelloWorld() {
-        Dialogs.showModalDialog(
-            DefaultDialogs.DIALOG_ID_INFO,
-            "hello",
-            "world"
-        );
+    function handleCursorSettings() {
+        // 
     }
-    
-      // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "helloworld.sayhello";   // package-style naming to avoid collisions
-    CommandManager.register("Hello World", MY_COMMAND_ID, handleHelloWorld);
 
-    // Then create a menu item bound to the command
-    // The label of the menu item is the name we gave the command (see above)
-    var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
-    
-    // We could also add a key binding at the same time:
-    //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
-    // (Note: "Ctrl" is automatically mapped to "Cmd" on Mac)
-    
+    // First, register a command - a UI-less object associating an id to a handler
+    const MY_COMMAND_ID = "cursor.settings";   // package-style naming to avoid collisions
+    CommandManager.register("Cursor Settings", MY_COMMAND_ID, handleCursorSettings);
+
+    StatusBar.addIndicator(
+        MY_COMMAND_ID,
+        $("<div>Cursor</div>").click(handleCursorSettings),
+        true, // show the indicator
+        "", // CSS class
+        "Switch between cursor settings", // tooltip text
+    );
+
     // Initialize extension once shell is finished initializing.
     AppInit.appReady(function () {
-        console.log("hello world");
+        console.log("'Cursor' extension loaded");
     });
 });
