3f94f203bf99f54daf9e3cea743a70ac787e23c3
Pluto <devvaannsh@gmail.com>
2024-12-09 13:49:15 +0530

    feat: open dialog box when cursor button is clicked

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Extension that is used for cursor settings, like changing the style of the cursor, the width of the cursor block or cursor blinking style
 define(function (require, exports, module) {
     "use strict";
     // Brackets modules
@@ -11,19 +10,50 @@ define(function (require, exports, module) {
 
     // Function to run when the menu item is clicked
     function handleCursorSettings() {
-        // 
+        // Create dialog HTML
+        const dialogHTML = `
+            <div class="cursor-settings-dialog">
+                <h3>Choose Cursor Style</h3>
+                <div>
+                    <label>
+                        <input type="radio" name="cursorStyle" value="bar" checked>
+                        Bar Cursor
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="radio" name="cursorStyle" value="underline">
+                        Underline Cursor
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="radio" name="cursorStyle" value="block">
+                        Block Cursor
+                    </label>
+                </div>
+            </div>
+        `;
+
+        // Show the dialog
+        Dialogs.showModalDialog(
+            DefaultDialogs.DIALOG_ID_INFO,
+            "Cursor Settings",
+            dialogHTML,
+        );
     }
 
     // First, register a command - a UI-less object associating an id to a handler
-    const MY_COMMAND_ID = "cursor.settings";   // package-style naming to avoid collisions
+    const MY_COMMAND_ID = "cursor.settings"; // package-style naming to avoid collisions
     CommandManager.register("Cursor Settings", MY_COMMAND_ID, handleCursorSettings);
 
+    // Add indicator to status bar
     StatusBar.addIndicator(
         MY_COMMAND_ID,
         $("<div>Cursor</div>").click(handleCursorSettings),
         true, // show the indicator
         "", // CSS class
-        "Switch between cursor settings", // tooltip text
+        "Switch between cursor settings" // tooltip text
     );
 
     // Initialize extension once shell is finished initializing.
